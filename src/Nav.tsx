15dba41eb28fdfdfb6dbfa3459103af559e99dd2
maciek5000 <maciek2baba2@gmail.com>
2022-11-20 19:00:53 +0100

    add use state hook in order to create hamburger menu animation

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SlPlane } from 'react-icons/sl'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 const Nav = () => {
+  const [nav, setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
   return (
     <div className='flex items-center mx-auto justify-beetwen max-w-[800px] text-white font-bold'>
-      <h2 className='h-10 w-full flex gap-3 uppercase m-2 text-xl text-green-600 items-center'>
-        Planes <SlPlane />
+      <h2 className='h-10 w-full flex gap-3 uppercase m-2 text-xl items-center'>
+        Planes <SlPlane className='text-green-600'/>
       </h2>
       <div className='flex hidden gap-5 items-center p-4'>
         <p className='hover:scale-110'>Home</p>
@@ -14,19 +20,19 @@ const Nav = () => {
         <p className='hover:scale-110'>Book</p>
         <p className='hover:scale-110'>About</p>
       </div>
-      <div>
-        <AiOutlineMenu size={30} />
+      <div onClick={handleNav}>
+        {nav ? < AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
       </div>
-      <div>
-        <ul className='fixed uppercase p-4 top-0 left-0 w-[30%] bg-black border-green-700 border-r h-full'>
+      <div className={nav ? 'fixed left-[-100%]' : 'fixed uppercase p-4 top-0 left-0 w-[40%] bg-black border-green-600 border-r h-full'}>
+        <ul>
           <li>
-            <h2 className='h-10 w-full flex gap-3 uppercase text-xl text-green-600 items-center'>
-              Planes <SlPlane />
+            <h2 className='h-10 w-full flex gap-3 uppercase text-xl text-white items-center'>
+              Planes <SlPlane className='text-green-600'/>
             </h2>
           </li>
-          <li className='hover:scale-110 border-b '>Home</li>
-          <li className='hover:scale-110 border-b '>Safety</li>
-          <li className='hover:scale-110 border-b '>Book</li>
+          <li className='hover:scale-110 border-b border-green-600'>Home</li>
+          <li className='hover:scale-110 border-b border-green-600'>Safety</li>
+          <li className='hover:scale-110 border-b border-green-600'>Book</li>
           <li className='hover:scale-110'>About</li>
         </ul>
       </div>
