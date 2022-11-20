858a73cbecca2e930647e00a2603044d5573b39f
maciek5000 <maciek2baba2@gmail.com>
2022-11-20 19:11:33 +0100

    finish nav component, fully implement hamburger menu functionality

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -3,7 +3,7 @@ import { SlPlane } from 'react-icons/sl'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 const Nav = () => {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(true)
 
   const handleNav = () => {
     setNav(!nav)
@@ -14,16 +14,16 @@ const Nav = () => {
       <h2 className='h-10 w-full flex gap-3 uppercase m-2 text-xl items-center'>
         Planes <SlPlane className='text-green-600'/>
       </h2>
-      <div className='flex hidden gap-5 items-center p-4'>
+      <div className='hidden md:flex gap-5 items-center p-4'>
         <p className='hover:scale-110'>Home</p>
         <p className='hover:scale-110'>Safety</p>
         <p className='hover:scale-110'>Book</p>
         <p className='hover:scale-110'>About</p>
       </div>
-      <div onClick={handleNav}>
+      <div onClick={handleNav} className='block md:hidden m-2'>
         {nav ? < AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
       </div>
-      <div className={nav ? 'fixed left-[-100%]' : 'fixed uppercase p-4 top-0 left-0 w-[40%] bg-black border-green-600 border-r h-full'}>
+      <div className={nav ? 'fixed left-[-100%] uppercase p-4 top-0 w-[40%] bg-black border-green-600 border-r h-full ease-in-out duration-300' : 'md:hidden fixed uppercase p-4 top-0 left-0 w-[40%] bg-black border-green-600 border-r h-full ease-in-out duration-300'}>
         <ul>
           <li>
             <h2 className='h-10 w-full flex gap-3 uppercase text-xl text-white items-center'>
