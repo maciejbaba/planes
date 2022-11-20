1cd78c722b1e512ff93a60d220136fa849acd4cc
maciek5000 <maciek2baba2@gmail.com>
2022-11-20 17:17:18 +0100

    add buttons in header

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { SlPlane } from 'react-icons/sl'
 
 const Nav = () => {
   return (
-    <div className='text-white'>nav</div>
+    <div className='flex items-center mx-auto justify-beetwen max-w-[800px] text-white font-bold'>
+      <h2 className='h-10 w-full flex p-4 uppercase m-2 text-xl text-green-600 items-center'>
+        Planes <SlPlane />
+      </h2>
+      <div className='flex gap-5 items-center p-4'> 
+        <button className='hover:scale-110'>Home</button>
+        <button className='hover:scale-110'>Safety</button>
+        <button className='hover:scale-110'>Book</button>
+        <button className='hover:scale-110'>About</button>
+      </div>
+    </div>
   )
 }
 
