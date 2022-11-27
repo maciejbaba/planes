f0873bad93c1fedc038a27f0198722092346e6c1
maciek367 <maciek2baba2@gmail.com>
2022-11-27 23:27:54 +0100

    add text to hero component

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col h-40 justify-center items-center'>
-      <p className='text-center'>Hero</p>
+    <div className='flex flex-col tracking-widest h-40 uppercase justify-center items-center font-bold'>
+      <p>fly with us</p>
+      <p>pls</p>
+      <p>we are the best</p>
     </div>
   )
 }
