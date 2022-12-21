5df006f6023780dc72aead6a0c2658776b1cf03f
maciek367 <maciek2baba2@gmail.com>
2022-12-21 22:44:04 +0100

    add another 2 photos, style them properly, extract class to single variable in order to not repeat yourself in classnames

--- src/ThreePhotos.tsx
+++ src/ThreePhotos.tsx
@@ -2,15 +2,31 @@ import React from 'react'
 
 type ImageType = string;
 
-const oldPlane: ImageType = require('./photos/old-plane.jpg')
+const oldPlanePath: ImageType = require('./photos/old-plane.jpg')
+const crashedPlanePath: ImageType = require('./photos/crashed-plane.jpg')
+const burningPlanePath: ImageType = require('./photos/burning-plane.jpg')
+
+const photoStyles = 'object-cover w-1/3 h-[15rem]'
 
 const ThreePhotos = () => {
   return (
-    <div>
+    <div className='flex m-7 space-x-7'>
       <img
-      src={oldPlane}
-      alt="Uncomfortably old plane"
-      className='object-scale-down'
+        src={oldPlanePath}
+        alt="Uncomfortably old plane"
+        className={`${photoStyles}`}
+      />
+
+      <img
+        src={crashedPlanePath}
+        alt="Uncomfortably old plane"
+        className={`${photoStyles}`}
+      />
+
+      <img
+        src={burningPlanePath}
+        alt="Uncomfortably old plane"
+        className={`${photoStyles}`}
       />
     </div>
   )
