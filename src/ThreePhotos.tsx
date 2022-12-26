ae50863a712c5335f8f5d6cee0d78c7a518996bb
maciek367 <maciek2baba2@gmail.com>
2022-12-26 20:20:22 +0100

    further development of three photos component, add captions to photos change organization - move from divs to figures

--- src/ThreePhotos.tsx
+++ src/ThreePhotos.tsx
@@ -6,28 +6,44 @@ const oldPlaneImgPath: ImagePathType = require('./photos/old-plane.jpg')
 const crashedPlaneImgPath: ImagePathType = require('./photos/crashed-plane.jpg')
 const burningPlaneImgPath: ImagePathType = require('./photos/burning-plane.jpg')
 
-const photoStyles = 'object-cover w-1/3 h-[15rem]'
+const photoStyles = 'object-cover w-full h-[15rem] rounded-xl'
+const photoDivStyles = 'w-1/3'
+const photoTextStyles = 'm-3 text-center bold text-xl'
 
 const ThreePhotos = () => {
   return (
     <div className='flex m-7 space-x-7'>
-      <img
-        src={oldPlaneImgPath}
-        alt="Uncomfortably old plane"
-        className={`${photoStyles}`}
-      />
+      <figure className={`${photoDivStyles}`}>
+        <img
+          src={oldPlaneImgPath}
+          alt="Uncomfortably old plane"
+          className={`${photoStyles}`}
+        />
+        <figcaption className={`${photoTextStyles}`}>
+          Our planes are incredibly old because we are incredibly cheap
+        </figcaption>
+      </figure>
 
-      <img
-        src={crashedPlaneImgPath}
-        alt="Uncomfortably old plane"
-        className={`${photoStyles}`}
-      />
+      <figure className={`${photoDivStyles}`}>
+        <img
+          src={crashedPlaneImgPath}
+          alt="Uncomfortably old plane"
+          className={`${photoStyles}`}
+        />
 
-      <img
-        src={burningPlaneImgPath}
-        alt="Uncomfortably old plane"
-        className={`${photoStyles}`}
-      />
+        <figcaption className={`${photoTextStyles}`}>
+          Almost every of our planes has crashed at least once, insurance companies don't like us
+        </figcaption>
+      </figure>
+
+      <figure className={`${photoDivStyles}`}>
+        <img
+          src={burningPlaneImgPath}
+          alt="Uncomfortably old plane"
+          className={`${photoStyles}`}
+        />
+        <figcaption className={`${photoTextStyles}`}>Very often our planes catch on fire because we don't care about your safety</figcaption>
+      </figure>
     </div>
   )
 }
